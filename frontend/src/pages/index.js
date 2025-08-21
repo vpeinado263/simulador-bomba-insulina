ba422402962848f528b4bad43cf7a88181aba321
Victor Peinado <vpeinado263@gmail.com>
2025-08-21 19:07:16 -0300

    Aplicar formato con Prettier a Frontend y Backend

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,10 +1,12 @@
 export default function Home() {
-    return (
-      <div className="min-h-screen flex flex-col items-center justify-center p-8">
-        <h1 className="text-3xl font-bold mb-4">Simulador de Bomba de Insulina</h1>
-        <p className="text-lg text-center">
-          Bienvenido, aquí se mostrará la lógica del algoritmo de la bomba.
-        </p>
-      </div>
-    );
-  }
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center p-8">
+      <h1 className="text-3xl font-bold mb-4">
+        Simulador de Bomba de Insulina
+      </h1>
+      <p className="text-lg text-center">
+        Bienvenido, aquí se mostrará la lógica del algoritmo de la bomba.
+      </p>
+    </div>
+  )
+}
