ec77d306cc37a40fa89095baae77ecc592e96cad
Victor Peinado <vpeinado263@gmail.com>
2025-08-23 19:20:46 -0300

    Configurar sppiner

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,12 +1,31 @@
-import InfusionCalculator from '@/components/InfusionCalculator'
+import SpinnerInicio from '@/components/atoms/SpinnerInicio';
+import InfusionCalculator from '@/components/InfusionCalculator';
+import { useState, useEffect } from 'react';
+
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, [])
+  
   return (
     <>
-      <h1 className="text-3xl font-bold mb-4">
+    {isLoading ? (
+      <SpinnerInicio />
+    ) : (
+      <div>
+        <h1 className="text-3xl font-bold mb-4">
         Simulador de Bomba de Insulina
       </h1>
       <InfusionCalculator />
+      </div>
+    )}
+      
     </>
   )
-}
+};
