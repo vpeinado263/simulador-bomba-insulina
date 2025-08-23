8ef14815dc3eedec10dd28bd344522cafbcdb83c
Victor Peinado <vpeinado263@gmail.com>
2025-08-23 18:57:57 -0300

    Crear componente Spinner, estructura atomic desing

---
+++ frontend/src/components/atoms/SpinnerInicio.js
@@ -0,0 +1,14 @@
+
+const SpinnerInicio = () => {
+  return (
+    <>
+      <div>
+        <h1>Protocolo de Infusión</h1>
+        <di></di>
+        <h2>Insulina</h2>
+      </div>
+    </>
+  )
+}
+
+export default SpinnerInicio;
