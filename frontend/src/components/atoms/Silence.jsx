672aef277fcd191f6eb94849986020171f2fc6db
Victor Peinado <vpeinado263@gmail.com>
2025-08-26 13:28:23 -0300

    Crear botón SILENCE

---
+++ frontend/src/components/atoms/Silence.jsx
@@ -0,0 +1,10 @@
+
+const Silence = () => {
+  return (
+    <button className="bg-gray-400 text-blue-950 p-2 rounded-full font-bold">
+      SILENCE  
+    </button>
+  );
+};
+
+export default Silence;
