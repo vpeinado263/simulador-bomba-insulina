0d66ac072a84ffd961e568a36cc0e6edc9684ec5
Victor Peinado <vpeinado263@gmail.com>
2025-08-26 15:37:41 -0300

    Crear botones Softkeys

---
+++ frontend/src/components/molecules/Softkeys.jsx
@@ -0,0 +1,22 @@
+
+const Softkeys = () => {
+    return (
+      <div className="inline-flex border-2 border-gray-500 rounded">
+        <button className="text-blue-950 font-bold text-xl w-12 h-12 flex items-center justify-center bg-gray-200 border-r-2 border-gray-500">
+          ▲
+        </button>
+        <button className="text-blue-950 font-bold text-xl w-12 h-12 flex items-center justify-center bg-gray-200 border-r-2 border-gray-500">
+          ▲
+        </button>
+        <button className="text-blue-950 font-bold text-xl w-12 h-12 flex items-center justify-center bg-gray-200 border-r-2 border-gray-500">
+          ▲
+        </button>
+        <button className="text-blue-950 font-bold text-xl w-12 h-12 flex items-center justify-center bg-gray-200">
+          ▲
+        </button>
+      </div>
+    );
+  };
+  
+  export default Softkeys;
+  
