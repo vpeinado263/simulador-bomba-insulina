d3d976e2f21a89ca6a74c5587aa974dd3fb6e856
Victor Peinado <vpeinado263@gmail.com>
2025-08-25 23:23:20 -0300

    Limpiar hoja primcipal index.js

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,29 +1,15 @@
-import SpinnerInicio from '@/components/atoms/SpinnerInicio';
 import InfusionCalculator from '@/components/InfusionCalculator';
-import { useState, useEffect } from 'react';
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 5000);
-    return () => clearTimeout(timer);
-  }, []);
 
   return (
     <>
-      {isLoading ? (
-        <SpinnerInicio />
-      ) : (
         <div>
           <h1 className="text-3xl font-bold mb-4">
             Simulador de Bomba de Insulina
           </h1>
           <InfusionCalculator />
         </div>
-      )}
     </>
   );
-}
+};
