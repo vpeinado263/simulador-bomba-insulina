180b396bb7af2eb9a3547493948825c71886251d
Victor Peinado <vpeinado263@gmail.com>
2025-08-26 18:56:21 -0300

    Crear componente WorkigRegion

---
+++ frontend/src/components/atoms/Pantallas y regiones básicas/WorkingRegion.jsx
@@ -0,0 +1,13 @@
+
+const WorkingRegion = () => {
+  return (
+    <div className="text-shadow-black-950 font-bold text-xl w-100 h-100 flex items-center justify-center bg-green-300">
+      40    Veloc.    0
+             ml/h
+       0    Vol. Inf. 0
+             ml.
+    </div>
+  );
+};
+
+export default WorkingRegion;
