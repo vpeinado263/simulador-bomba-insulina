89e629192030f71ba51d578c41e06b3bee692f31
Victor Peinado <vpeinado263@gmail.com>
2025-08-26 00:07:57 -0300

    feat: agregar spinner global en primera carga de la app

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,14 +1,24 @@
 import Head from 'next/head';
 import '../styles/globals.css';
+import { useState, useEffect } from 'react';
+import SpinnerInicio from '@/components/atoms/SpinnerInicio';
 
 export default function App({ Component, pageProps }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 1500);
+    return () => clearTimeout(timer);
+  }, []);
+  
   return (
     <>
       <Head>
         <title>Bomba de Insulina</title>
         <link rel="icon" href="/nurshing.ico" />
       </Head>
-      <Component {...pageProps} />
+
+      {loading ? <SpinnerInicio /> : <Component {...pageProps} />}
     </>
   );
-}
+};
