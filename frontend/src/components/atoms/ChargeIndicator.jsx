3b5c5db4fdaeb1116e03841a280463cc965d6785
Victor Peinado <vpeinado263@gmail.com>
2025-08-26 16:16:45 -0300

    Crear componente ChargerIndicator

---
+++ frontend/src/components/atoms/ChargeIndicator.jsx
@@ -0,0 +1,11 @@
+
+const ChargeIndicator = () => {
+  return (
+    <div className="flex items-center gap-5"><span role="img" aria-label="plug">
+      🔌
+    </span>
+    </div>
+  );
+};
+
+export default ChargeIndicator;
