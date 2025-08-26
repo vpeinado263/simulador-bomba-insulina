ecc910d27f8037acf2fba27da7469030204a5a94
Victor Peinado <vpeinado263@gmail.com>
2025-08-26 14:25:18 -0300

    Crear botón SELECT

---
+++ frontend/src/components/atoms/Select.jsx
@@ -0,0 +1,20 @@
+
+
+const Select = () => {
+    return (
+      <div className="flex flex-col items-center bg-gray-400 p-2 rounded-full w-16">
+        <button className="text-blue-950 font-bold text-xl py-1 w-full">
+          ▲
+        </button>
+        <span className="text-blue-950 font-bold text-sm py-1">
+          SELECT
+        </span>
+        <button className="text-blue-950 font-bold text-xl py-1 w-full">
+          ▼
+        </button>
+      </div>
+    );
+  };
+  
+  export default Select;
+  
