d3daae929916087e0f8cf115889f89f06796145e
Victor Peinado <vpeinado263@gmail.com>
2025-08-26 17:18:35 -0300

    Crear componente BatteryCapacitySymbol

---
+++ frontend/src/components/atoms/BatteryCapacitySymbol.jsx
@@ -0,0 +1,13 @@
+
+const BatteryCapacitySymbol = () => {
+    return (
+      <div className="flex items-center gap-2">
+        <span role="img" aria-label="battery full">
+          🔋
+        </span>
+      </div>
+    );
+  };
+  
+  export default BatteryCapacitySymbol;
+  
