e42ca668e5710980f5d26e8f2aa081ac8d8125e4
Victor Peinado <vpeinado263@gmail.com>
2025-08-26 13:12:52 -0300

    Crear botón On/Off

---
+++ frontend/src/components/atoms/OnOff.jsx
@@ -0,0 +1,10 @@
+
+const OnOff = () => {
+  return (
+    <button className="bg-yellow-400 text-blue-950 p-2 rounded-full font-bold">
+      ON/OFF  
+    </button>
+  );
+};
+
+export default OnOff;
