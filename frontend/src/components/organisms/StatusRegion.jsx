2f54c904e7a6de45ce10cf2a863b5a6f2c6b38cf
Victor Peinado <vpeinado263@gmail.com>
2025-08-27 00:30:19 -0300

    Crear componente StatusRegion

---
+++ frontend/src/components/organisms/StatusRegion.jsx
@@ -0,0 +1,15 @@
+
+const StatusRegion = () => {
+  return (
+    <div className="w-full flex justify-between items-center mb-2">
+      <div className="flex gap-2">
+        <span className="w-6 h-6 bg-gray-400 rounded"></span> 
+        <span className="w-6 h-6 bg-gray-400 rounded"></span> 
+        <span className="w-6 h-6 bg-gray-400 rounded"></span>
+      </div>
+      <p className="text-sm">Indicadores</p>
+    </div>
+  );
+};
+
+export default StatusRegion;
