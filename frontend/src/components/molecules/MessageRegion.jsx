87b961a35d34cb69f624bf48352f052729d95ab2
Victor Peinado <vpeinado263@gmail.com>
2025-08-27 01:16:40 -0300

    Agregar Indicador Batery

--- frontend/src/components/molecules/MessageRegion.jsx
+++ frontend/src/components/molecules/MessageRegion.jsx
@@ -1,8 +1,9 @@
+import BatteryCapacitySymbol from "../atoms/Indicadores/BatteryCapacitySymbol";
 
 const MessageRegion = () => {
   return (
   <div className="w-full h-12 bg-red-200 text-red-700 flex items-center justify-center rounded mt-4">
-    <p>Sin alertas</p>
+    <BatteryCapacitySymbol />
   </div>
   );
 };
