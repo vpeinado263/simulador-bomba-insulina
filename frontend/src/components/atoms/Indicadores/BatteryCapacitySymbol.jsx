cf643dbc3310e67ba51b8bc862e97bb16b8d92bc
Victor Peinado <vpeinado263@gmail.com>
2025-08-27 12:26:08 -0300

    Editar tamaño de archivo svg

--- frontend/src/components/atoms/Indicadores/BatteryCapacitySymbol.jsx
+++ frontend/src/components/atoms/Indicadores/BatteryCapacitySymbol.jsx
@@ -3,7 +3,7 @@ const BatteryCapacitySymbol = () => {
     return (
       <div className="flex items-center gap-2">
         <div className="flex items-center gap-5">
-        <img src="/batery.svg" alt="Charge Indicator" width={140} height={96} />
+        <img src="/batery.svg" alt="Charge Indicator" width={40} height={96} />
       </div>
       </div>
     );
