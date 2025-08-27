a39fab5031a76d2613072d0df0e5ce17f6b163b6
Victor Peinado <vpeinado263@gmail.com>
2025-08-27 01:14:53 -0300

    Insertar MessageRegion en LCD

--- frontend/src/components/organisms/Lcd.jsx
+++ frontend/src/components/organisms/Lcd.jsx
@@ -1,9 +1,17 @@
+import MessageRegion from "../molecules/MessageRegion"
 import StatusRegion from "../molecules/StatusRegion"
 
 const Lcd = () => {
   return (
     <div className="w-full h-60 bg-green-300 rounded-md ">
       <StatusRegion />
+      <div>00.00</div>
+      <div>00.00</div>
+      <div>00.00</div>
+      <div>00.00</div>
+      <div>00.00</div>
+      <div>00.00</div>
+      <MessageRegion />
     </div>
   )
 }
