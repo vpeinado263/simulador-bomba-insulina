22133c37bec8b626758b64f2fd53b49f153b008c
Victor Peinado <vpeinado263@gmail.com>
2025-08-27 12:08:57 -0300

    Estilizar componentes

--- frontend/src/components/organisms/Lcd.jsx
+++ frontend/src/components/organisms/Lcd.jsx
@@ -1,22 +1,19 @@
 import MessageRegion from "../molecules/MessageRegion"
 import SoftkeyLabel from "../molecules/SoftkeyLabel"
 import StatusRegion from "../molecules/StatusRegion"
-import ControlPanel from "./ControlPanel"
+
 
 const Lcd = () => {
   return (
-    <div className="w-full h-75 bg-green-300 rounded-md ">
+    <>
+    <div>
+      <div className="w-full h-75 bg-green-300 rounded-md ">
       <StatusRegion />
-      <div>00.00</div>
-      <div>00.00</div>
-      <div>00.00</div>
-      <div>00.00</div>
-      <div>00.00</div>
-      <div>00.00</div>
       <MessageRegion />
       <SoftkeyLabel />
-      <ControlPanel />
+      </div>
     </div>
+    </>
   )
 }
 
--- frontend/src/components/templates/DeviceLayout.jsx
+++ frontend/src/components/templates/DeviceLayout.jsx
@@ -4,8 +4,10 @@ import Lcd from "../organisms/Lcd";
 const DeviceLayout = () => {
   return (
     <div className="w-[600px] h-[800px] bg-blue-950 rounded-2xl shadow-lg flex flex-col items-center p-4">
-      <Lcd /> 
-      <Softkeys />
+      <Lcd />
+      <div>
+       <Softkeys />
+      </div> 
     <div className="flex-1 w-full flex items-center justify-center my-2">  
 
     </div>
