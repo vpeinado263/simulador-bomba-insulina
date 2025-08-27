bcb1be92ce5003d948e5462e6a0329388bcb10fd
Victor Peinado <vpeinado263@gmail.com>
2025-08-27 01:11:57 -0300

    Crear componente MessageRegion

---
+++ frontend/src/components/molecules/MessageRegion.jsx
@@ -0,0 +1,10 @@
+
+const MessageRegion = () => {
+  return (
+  <div className="w-full h-12 bg-red-200 text-red-700 flex items-center justify-center rounded mt-4">
+    <p>Sin alertas</p>
+  </div>
+  );
+};
+
+export default MessageRegion;
