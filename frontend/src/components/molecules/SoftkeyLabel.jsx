a3733647d15cb26ba6dca18c1ac84ee4a64e62cc
Victor Peinado <vpeinado263@gmail.com>
2025-08-27 01:23:53 -0300

    Crear componente SoftkeyLabel

---
+++ frontend/src/components/molecules/SoftkeyLabel.jsx
@@ -0,0 +1,15 @@
+
+const SoftkeyLabel = () => {
+  return (
+    <div className="w-full flex justify-between items-center mb-2">
+    <div className="flex gap-2">
+      <span className="w-20 h-12 bg-gray-400 rounded">Back Prime</span> 
+      <span className="w-20 h-12 bg-gray-400 rounded">A</span> 
+      <span className="w-20 h-12 bg-gray-400 rounded">B</span>
+      <span className="w-20 h-12 bg-gray-400 rounded">Options/Vol Inf.</span>
+    </div>
+  </div>
+  );
+};
+
+export default SoftkeyLabel;
--- frontend/src/components/organisms/Lcd.jsx
+++ frontend/src/components/organisms/Lcd.jsx
@@ -1,9 +1,10 @@
 import MessageRegion from "../molecules/MessageRegion"
+import SoftkeyLabel from "../molecules/SoftkeyLabel"
 import StatusRegion from "../molecules/StatusRegion"
 
 const Lcd = () => {
   return (
-    <div className="w-full h-60 bg-green-300 rounded-md ">
+    <div className="w-full h-75 bg-green-300 rounded-md ">
       <StatusRegion />
       <div>00.00</div>
       <div>00.00</div>
@@ -12,6 +13,7 @@ const Lcd = () => {
       <div>00.00</div>
       <div>00.00</div>
       <MessageRegion />
+      <SoftkeyLabel />
     </div>
   )
 }
