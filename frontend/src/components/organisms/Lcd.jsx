b67fb8b25b0a988b89751694f8c7c4ef36166510
Victor Peinado <vpeinado263@gmail.com>
2025-08-27 01:09:22 -0300

    Insertar StatusRegion en LCD

---
+++ frontend/src/components/organisms/Lcd.jsx
@@ -0,0 +1,11 @@
+import StatusRegion from "../molecules/StatusRegion"
+
+const Lcd = () => {
+  return (
+    <div className="w-full h-60 bg-green-300 rounded-md ">
+      <StatusRegion />
+    </div>
+  )
+}
+
+export default Lcd
--- frontend/src/components/templates/DeviceLayout.jsx
+++ frontend/src/components/templates/DeviceLayout.jsx
@@ -1,10 +1,9 @@
-import StatusRegion from "../organisms/StatusRegion";
-
+import Lcd from "../organisms/Lcd";
 
 const DeviceLayout = () => {
   return (
     <div className="w-[400px] h-[600px] bg-gray-200 rounded-2xl shadow-lg flex flex-col items-center p-4">
-        <StatusRegion />
+      <Lcd /> 
     <div className="flex-1 w-full flex items-center justify-center my-2">   
     </div>
   </div>
