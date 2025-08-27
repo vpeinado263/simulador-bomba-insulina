e3a70a5a0c4de375f319cffa385b3e8517ddce00
Victor Peinado <vpeinado263@gmail.com>
2025-08-27 14:00:13 -0300

    Estilizar componentes en LCD

--- frontend/src/components/organisms/Lcd.jsx
+++ frontend/src/components/organisms/Lcd.jsx
@@ -2,19 +2,30 @@ import MessageRegion from "../molecules/MessageRegion"
 import SoftkeyLabel from "../molecules/SoftkeyLabel"
 import StatusRegion from "../molecules/StatusRegion"
 
-
 const Lcd = () => {
   return (
-    <>
-    <div>
-      <div className="w-full h-75 bg-green-300 rounded-md ">
-      <StatusRegion />
-      <MessageRegion />
-      <SoftkeyLabel />
+    <div className="w-full h-80 bg-green-200 flex flex-col justify-between">
+      <div className="p-2  font-bold border-b border-black">
+        <StatusRegion />
+      </div>
+      <div className="flex-1 flex flex-col justify-center items-center space-y-6">
+        <div className="flex flex-col items-center">
+          <span className="text-3xl font-extrabold text-black">125</span>
+          <span className="text-sm text-gray-800">mL/hr</span>
+        </div>
+        <div className="flex flex-col items-center">
+          <span className="text-2xl font-extrabold text-black">450</span>
+          <span className="text-sm text-gray-800">mL</span>
+        </div>
+      </div>
+      <div>
+        <MessageRegion />
+      </div>
+      <div>
+        <SoftkeyLabel />
       </div>
     </div>
-    </>
-  )
-}
+  );
+};
 
-export default Lcd
+export default Lcd;
