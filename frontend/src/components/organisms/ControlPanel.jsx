8255229f6059ed75a891284d43f26fe2596a678e
Victor Peinado <vpeinado263@gmail.com>
2025-08-29 10:14:42 -0300

    Insertar componente Molecule

--- frontend/src/components/organisms/ControlPanel.jsx
+++ frontend/src/components/organisms/ControlPanel.jsx
@@ -1,9 +1,5 @@
-import OnOff from "../atoms/Botones Individuales/OnOff";
 import Select from "../atoms/Botones Individuales/Select";
 import Silence from "../atoms/Botones Individuales/Silence";
-import Start from "../atoms/Botones Individuales/Start";
-import Stop from "../atoms/Botones Individuales/Stop";
-import ChargeIndicator from "../atoms/Indicadores/ChargeIndicator";
 import NumericKeys from "../molecules/NumericKeys";
 
 const ControlPanel = () => {
@@ -11,10 +7,7 @@ const ControlPanel = () => {
     <>
     <div className="w-full flex justify-between items-center mt-6">
       <div className="flex flex-col gap-10">
-        <Start />
-        <Stop />
-        <ChargeIndicator />
-        <OnOff />
+     
       </div>
       <div className="flex justify-center">
         <NumericKeys />
