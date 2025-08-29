0c461208706f24dcfe000486b223b10e5c4cac97
Victor Peinado <vpeinado263@gmail.com>
2025-08-29 10:15:44 -0300

    Insertar componente Molecular

--- frontend/src/components/organisms/ControlPanel.jsx
+++ frontend/src/components/organisms/ControlPanel.jsx
@@ -1,5 +1,6 @@
 import Select from "../atoms/Botones Individuales/Select";
 import Silence from "../atoms/Botones Individuales/Silence";
+import ActionButtonsUno from "../molecules/ActionButtonsUno";
 import NumericKeys from "../molecules/NumericKeys";
 
 const ControlPanel = () => {
@@ -7,7 +8,7 @@ const ControlPanel = () => {
     <>
     <div className="w-full flex justify-between items-center mt-6">
       <div className="flex flex-col gap-10">
-     
+        <ActionButtonsUno />
       </div>
       <div className="flex justify-center">
         <NumericKeys />
