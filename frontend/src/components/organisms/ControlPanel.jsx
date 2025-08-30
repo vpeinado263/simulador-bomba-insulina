d7aeb7daae203db41f3284295d923dd16236cd95
Victor Peinado <vpeinado263@gmail.com>
2025-08-30 13:52:08 -0300

    Agregar componente ActionsButtonsDos

--- frontend/src/components/organisms/ControlPanel.jsx
+++ frontend/src/components/organisms/ControlPanel.jsx
@@ -1,21 +1,19 @@
-import Select from "../atoms/Botones Individuales/Select";
-import Silence from "../atoms/Botones Individuales/Silence";
 import ActionButtonsUno from "../molecules/ActionButtonsUno";
+import ActionsButtonsDos from "../molecules/ActionsButtonsDos";
 import NumericKeys from "../molecules/NumericKeys";
 
 const ControlPanel = () => {
   return (
     <>
     <div className="w-full flex justify-between items-center mt-6">
-      <div className="flex flex-col gap-10">
+      <div className="flex flex-col gap-6">
         <ActionButtonsUno />
       </div>
       <div className="flex justify-center">
         <NumericKeys />
       </div>
       <div className="flex flex-col gap-5 items-center">
-        <Select />
-        <Silence />
+        <ActionsButtonsDos />
       </div>
     </div>
     </>
