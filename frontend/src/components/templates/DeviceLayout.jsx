8d60ab0ec6e99ebb8a7337c450cb956d71c8a311
Victor Peinado <vpeinado263@gmail.com>
2025-08-30 15:12:29 -0300

    Editar estilos en componentes

--- frontend/src/components/templates/DeviceLayout.jsx
+++ frontend/src/components/templates/DeviceLayout.jsx
@@ -5,15 +5,17 @@ import Lcd from "../organisms/Lcd";
 
 const DeviceLayout = () => {
   return (
-    <div className="w-[600px] h-[800px] bg-blue-950 rounded-2xl shadow-lg flex flex-col justify-between items-center p-6">
+    <div className="w-[800px] h-[800px] bg-blue-950 rounded-2xl shadow-lg flex flex-col justify-between items-center p-8">
       <div>
         <FluidIndicators />
       </div>
-      <div className="w-full flex flex-col items-center my-4">
+      <div>
         <Lcd />
+      </div>
+      <div>
         <Softkeys />
       </div>
-      <div className="w-full flex-1 flex items-end justify-center mt-4">
+      <div>
         <ControlPanel />
       </div>
     </div>
