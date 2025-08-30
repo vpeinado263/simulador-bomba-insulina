c9093c335f2f3aa445d85a7b0e1c3ec4ef08ce89
Victor Peinado <vpeinado263@gmail.com>
2025-08-30 14:36:50 -0300

    Insertar componente FluidIndicator

--- frontend/src/components/templates/DeviceLayout.jsx
+++ frontend/src/components/templates/DeviceLayout.jsx
@@ -1,18 +1,22 @@
 import Softkeys from "../atoms/Botones Individuales/Softkeys";
+import FluidIndicators from "../molecules/FluidIndicators";
 import ControlPanel from "../organisms/ControlPanel";
 import Lcd from "../organisms/Lcd";
 
 const DeviceLayout = () => {
   return (
-    <div className="w-[600px] h-[800px] bg-blue-950 rounded-2xl shadow-lg flex flex-col items-center p-4">
-      <Lcd />
+    <div className="w-[600px] h-[800px] bg-blue-950 rounded-2xl shadow-lg flex flex-col justify-between items-center p-6">
       <div>
-       <Softkeys />
-      </div> 
-    <div className="flex-1 w-full flex items-center justify-center my-2">  
-    <ControlPanel />
+        <FluidIndicators />
+      </div>
+      <div className="w-full flex flex-col items-center my-4">
+        <Lcd />
+        <Softkeys />
+      </div>
+      <div className="w-full flex-1 flex items-end justify-center mt-4">
+        <ControlPanel />
+      </div>
     </div>
-  </div>
   );
 };
 
