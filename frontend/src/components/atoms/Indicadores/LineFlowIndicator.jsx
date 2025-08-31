da4c48f4de72058aa428e533a07b69a2aa4565dd
Victor Peinado <vpeinado263@gmail.com>
2025-08-31 13:35:15 -0300

    Agregar tamaño LineFlowIndicator

--- frontend/src/components/atoms/Indicadores/LineFlowIndicator.jsx
+++ frontend/src/components/atoms/Indicadores/LineFlowIndicator.jsx
@@ -3,7 +3,7 @@ const LineFlowIndicator = () => {
   return (
     <div className="relative flex items-center justify-center">
       <img 
-      src="" 
+      src="lineflowindicator.svg" 
       alt="Line Flow Indicator"
       width={40}
       height={95}
