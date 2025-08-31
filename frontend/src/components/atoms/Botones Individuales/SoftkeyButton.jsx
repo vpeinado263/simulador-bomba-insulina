418674dd350ceacd04b282207fdbf2445d7eb71a
Victor Peinado <vpeinado263@gmail.com>
2025-08-31 18:23:23 -0300

    feat(ui): agregar componente SoftkeyButton con icono SVG y soporte de etiqueta

--- frontend/src/components/atoms/Botones Individuales/SoftkeyButton.jsx
+++ frontend/src/components/atoms/Botones Individuales/SoftkeyButton.jsx
@@ -1,8 +1,8 @@
 const SoftkeyButton = ({ label }) => {
   return (
-    <div className="w-15 h-10 flex items-center justify-center bg-gray-200 border-r-2 border-gray-500">
-      <button className="text-blue-950 font-bold flex items-center gap-1">
-        <span className="text-2xl">▲</span>
+    <div className="w-15 h-10 flex items-center justify-center bg-gray-200">
+      <button>
+      <img src="/softkeybutton.svg" alt="Botón softkey" width={60} height={50} />
         {label}
       </button>
     </div>
