9f7ea90662cc11c3126c3fe0acf5a385dd1da5ed
Victor Peinado <vpeinado263@gmail.com>
2025-08-31 16:01:59 -0300

    Editar botón atómico

--- frontend/src/components/atoms/Botones Individuales/SoftkeyButton.jsx
+++ frontend/src/components/atoms/Botones Individuales/SoftkeyButton.jsx
@@ -1,13 +1,14 @@
 
-const SoftkeyButton = ({onClick, label}) => {
-  return (
-    <button
-      onClick={onClick}
-      className="w-12 h-12 flex items-center justify-center border-2 border-gray-500 rounded bg-gray-200 text-blue-950 font-bold text-xl"
-    >
-      {label}
-    </button>
-  );
-};
-
-export default SoftkeyButton;
+const SoftkeyButton = ({ onClick }) => {
+    return (
+      <button
+        onClick={onClick}
+        className="w-12 h-12 flex items-center justify-center border-2 border-gray-500 rounded bg-gray-200 text-blue-950 font-bold text-xl"
+      >
+        ▲
+      </button>
+    );
+  };
+  
+  export default SoftkeyButton;
+  
