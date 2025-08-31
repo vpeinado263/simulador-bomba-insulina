ed51d5fcd36d548a4008e1679f07cdee107a9987
Victor Peinado <vpeinado263@gmail.com>
2025-08-31 12:46:07 -0300

    Crear componente atómico LineFlowIndicator

---
+++ frontend/src/components/atoms/Indicadores/LineFlowIndicator.jsx
@@ -0,0 +1,15 @@
+
+const LineFlowIndicator = () => {
+  return (
+    <div className="relative flex items-center justify-center">
+      <img 
+      src="" 
+      alt="Line Flow Indicator"
+      width={40}
+      height={95}
+      />
+    </div>
+  );
+};
+
+export default LineFlowIndicator;
