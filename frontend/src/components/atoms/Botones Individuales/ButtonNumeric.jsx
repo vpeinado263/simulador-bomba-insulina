13c3005a661ec95a36ecd5b20fab588caa362fcc
Victor Peinado <vpeinado263@gmail.com>
2025-09-01 21:56:36 -0300

    Modificar tamaño de teclanumérico

--- frontend/src/components/atoms/Botones Individuales/ButtonNumeric.jsx
+++ frontend/src/components/atoms/Botones Individuales/ButtonNumeric.jsx
@@ -3,7 +3,7 @@ const ButtonNumeric = ({label, onClick}) => {
   <button 
   onClick={onClick}  
   className="bg-white hover:bg-gray-300 text-blue-950 font-bold 
-  rounded-full  py-3 flex items-center justify-center text-sm">
+  rounded-full  py-2 flex items-center justify-center text-sm">
     {label}
   </button>
 
