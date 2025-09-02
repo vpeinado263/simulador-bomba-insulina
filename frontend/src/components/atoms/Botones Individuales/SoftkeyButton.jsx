80b2c5a9e9ef11bb27766ebcf7aba8d40713157f
Victor Peinado <vpeinado263@gmail.com>
2025-09-01 21:53:05 -0300

    Borrar error de tipeo

--- frontend/src/components/atoms/Botones Individuales/SoftkeyButton.jsx
+++ frontend/src/components/atoms/Botones Individuales/SoftkeyButton.jsx
@@ -1,6 +1,6 @@
 const SoftkeyButton = ({ label }) => {
   return (
-    <div className="w-14------- h-7 flex items-center justify-center bg-gray-200">
+    <div className="w-14 h-7 flex items-center justify-center bg-gray-200">
       <button>
       <img src="/softkeybutton.svg" alt="Botón softkey" width={45} height={45} />
         {label}
