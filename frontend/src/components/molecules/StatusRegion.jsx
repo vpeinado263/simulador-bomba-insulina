8e9683e389c2f778674dec1596c5fa2d9e7a4798
Victor Peinado <vpeinado263@gmail.com>
2025-09-01 21:20:55 -0300

    Editar stauts inicial

--- frontend/src/components/molecules/StatusRegion.jsx
+++ frontend/src/components/molecules/StatusRegion.jsx
@@ -3,16 +3,7 @@ const StatusRegion = () => {
     <div>
       <div className="flex items-center">
         <span className="flex-[1] h-8 bg-green-300 flex justify-center border border-black">
-          A
-        </span>
-        <span className="flex-[4] h-8 bg-gray-400 flex justify-center border border-black">
-          STOPED
-        </span>
-        <span className="flex-[4] h-8 bg-gray-400 flex justify-center border border-black">
-          STOPED
-        </span>
-        <span className="flex-[1] h-8 bg-gray-400 flex justify-center border border-black">
-          B
+          ABBOTT Plum A+
         </span>
       </div>
     </div>
