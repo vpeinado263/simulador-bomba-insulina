747b7f4cd9350a2365e34fab80b888981562a6a6
Victor Peinado <vpeinado263@gmail.com>
2025-09-03 12:58:54 -0300

    Conectar botón OnOff

--- frontend/src/components/atoms/Botones Individuales/OnOff.jsx
+++ frontend/src/components/atoms/Botones Individuales/OnOff.jsx
@@ -1,6 +1,12 @@
+import { useDevice } from "@/contexts/DeviceContext";
+
 const OnOff = () => {
+ const { togglePower } = useDevice();
+
   return (
-    <button className="bg-yellow-400  hover:bg-yellow-600 text-blue-950 rounded-full font-bold w-18 h-8 text-sm">
+    <button 
+    onClick={ togglePower } 
+    className="bg-yellow-400  hover:bg-yellow-600 text-blue-950 rounded-full font-bold w-18 h-8 text-sm">
       ON/OFF  
     </button>
   );
