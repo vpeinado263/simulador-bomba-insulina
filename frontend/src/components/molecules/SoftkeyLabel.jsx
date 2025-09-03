bd027c6183b6208e4ec6817a778ab17552f1dcec
Victor Peinado <vpeinado263@gmail.com>
2025-09-02 21:52:12 -0300

    Editar color de bordes

--- frontend/src/components/molecules/SoftkeyLabel.jsx
+++ frontend/src/components/molecules/SoftkeyLabel.jsx
@@ -1,10 +1,10 @@
 const SoftkeyLabel = () => {
   return (
     <div className="flex justify-between">
-      <span className="w-18 h-9 bg-green-300 border"></span> 
-      <span className="w-18 h-9 bg-green-300 border"></span> 
-      <span className="w-18 h-9 bg-green-300 border"></span>
-      <span className="w-18 h-9 bg-green-300 border"></span>
+      <span className="w-18 h-9 bg-green-200 border border-green-600"></span> 
+      <span className="w-18 h-9 bg-green-200 border border-green-600"></span> 
+      <span className="w-18 h-9 bg-green-200 border border-green-600"></span>
+      <span className="w-18 h-9 bg-green-200 border border-green-600"></span>
   </div>
   );
 };
--- frontend/src/components/molecules/StatusRegion.jsx
+++ frontend/src/components/molecules/StatusRegion.jsx
@@ -2,7 +2,7 @@ const StatusRegion = () => {
   return (
     <div>
       <div className="flex items-center">
-        <span className="flex-[1] h-8 bg-green-300 flex justify-center border border-black">
+        <span className="flex-[1] h-8 bg-green-300 flex justify-center border border-green-600 text-gray-600">
           HOSPIRA Plum A+
         </span>
       </div>
