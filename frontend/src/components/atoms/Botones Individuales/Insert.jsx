e6f3c1ebc56483ebec2e4a89f3e309792b725660
Victor Peinado <vpeinado263@gmail.com>
2025-09-03 19:24:33 -0300

    Insertar estilos personalizados a botón

--- frontend/src/components/atoms/Botones Individuales/Insert.jsx
+++ frontend/src/components/atoms/Botones Individuales/Insert.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-
 const Insert = () => {
   return (
-    <div>
-        <button>Insertar PLUM set</button>
-    </div>
+    <button className="w-40 h-12 bg-green-600 text-white font-bold rounded-md border-2 border-gray-800 shadow-md hover:bg-green-700 active:scale-95 active:shadow-inner transition">
+      Insertar PLUM set
+    </button>
   );
 };
 
