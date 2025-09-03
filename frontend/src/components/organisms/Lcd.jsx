1888b0e71a2e6124112e8eb5e39d2ebf041b8e8a
Victor Peinado <vpeinado263@gmail.com>
2025-09-03 14:28:18 -0300

    Insertar componente modular StartuoScreen

--- frontend/src/components/organisms/Lcd.jsx
+++ frontend/src/components/organisms/Lcd.jsx
@@ -2,6 +2,7 @@ import { useDevice } from "@/contexts/DeviceContext";
 import MessageRegion from "../molecules/MessageRegion";
 import SoftkeyLabel from "../molecules/SoftkeyLabel";
 import StatusRegion from "../molecules/StatusRegion";
+import StartupScreen from "./screens/StartupScreen";
 
 const Lcd = () => {
   const { isOn, loading } = useDevice();
@@ -11,27 +12,7 @@ const Lcd = () => {
 
   if (loading) {
     return (
-      <div className="w-64 h-60 bg-green-200 flex flex-col justify-between rounded-md">
-        <div>
-          <StatusRegion />
-        </div>
-        <div className="flex-1 flex flex-col justify-center items-center text-sm">
-          <div className="flex flex-col items-center mb-2">
-            <span>Ver: 13.43.00.001 2014/11/20</span>
-            <span>Copyright Hospira Inc.</span>
-          </div>
-          <div className="flex flex-col items-center font-bold">
-            <span>System Self Test</span>
-            <span>In Progress</span>
-          </div>
-        </div>
-        <div>
-          <MessageRegion />
-        </div>
-        <div>
-          <SoftkeyLabel />
-        </div>
-      </div>
+     <StartupScreen />
     );
   }
 
