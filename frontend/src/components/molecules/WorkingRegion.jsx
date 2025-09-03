b6448a8a8bca1eef597e382dd63cde59de962ad3
Victor Peinado <vpeinado263@gmail.com>
2025-09-03 15:21:00 -0300

    Crear componente WorkingRegion

---
+++ frontend/src/components/molecules/WorkingRegion.jsx
@@ -0,0 +1,17 @@
+
+const WorkingRegion = () => {
+  return (
+    <div className="flex-1 flex flex-col justify-center items-center text-sm">
+          <div className="flex flex-col items-center mb-2">
+            <span></span>
+            <span></span>
+          </div>
+          <div className="flex flex-col items-center font-bold">
+            <span></span>
+            <span></span>
+          </div>
+    </div>
+  );
+};
+
+export default WorkingRegion;
