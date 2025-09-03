8a6ce0d36a5fb253a26a5a523e3768c548fd9f15
Victor Peinado <vpeinado263@gmail.com>
2025-09-03 12:48:34 -0300

    Crear contexto global DeviceContext

---
+++ frontend/src/contexts/DeviceContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useContext, useState } from "react";
+
+const DeviceContext = createContext ();
+
+export const useDevice = () => useContext(DeviceContext);
+
+export const DeviceProvider = ({ children }) => {
+  const [isOn, setIsOn] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+const togglePower = () => {
+  if (!OsOn) {
+    setLoading(true);
+    setTimeout(() => {
+      setIsOn(true);
+      setLoading(false);
+    }, 3000);
+  } else {
+    setIsOn(false);
+    setLoading(false);
+  }
+};
+
+return (
+    <DeviceContext.Provider value={{ isOn, loading, togglePower }}>
+      {children} 
+    </DeviceContext.Provider>
+  );
+};
