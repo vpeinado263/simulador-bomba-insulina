b0f54aa3d77c6be3b4b9e07deb6d799daef84ebf
Victor Peinado <vpeinado263@gmail.com>
2025-09-03 19:23:03 -0300

    Crear estilos personalizados

--- frontend/src/components/atoms/Botones Individuales/Close.jsx
+++ frontend/src/components/atoms/Botones Individuales/Close.jsx
@@ -1,8 +1,8 @@
 const Close = () => {
   return (
-    <div>
-        <button>Close Lever</button>  
-    </div>
+    <button className="w-40 h-12 bg-red-600 text-white font-bold rounded-md border-2 border-gray-800 shadow-md hover:bg-red-700 active:scale-95 active:shadow-inner transition">
+      Close Lever
+    </button>  
   );
 };
 
