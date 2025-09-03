1c21dec65ae88d49f7cf1669382d529627d7225e
Victor Peinado <vpeinado263@gmail.com>
2025-09-02 21:39:38 -0300

    Incorporar indicador led

--- frontend/src/components/atoms/Indicadores/ChargeIndicator.jsx
+++ frontend/src/components/atoms/Indicadores/ChargeIndicator.jsx
@@ -1,19 +1,13 @@
-const ChargeIndicator = ({ active }) => {
+const ChargeIndicator = () => {
   return (
-    <div className="relative w-[70px] h-[30px] flex items-center justify-center">
+    <div className="relative w-[70px] h-[30px] flex items-center justify-center space-x-2">
       <img
         src="/enchufe.svg"
         alt="Charge Indicator"
         width={60}
         height={60}
       />
-      <div
-        className={`absolute w-3 h-3 rounded-full transition-all duration-500 ease-in-out
-          ${active 
-            ? "bg-yellow-300 shadow-[0_0_8px_2px_rgba(250,204,21,0.6)]" 
-            : "bg-yellow-500 shadow-none"                               
-          }`}
-      />
+      <div className="absolute w-3 h-3 rounded-full bg-yellow-600"/>
     </div>
   );
 };
