c8b7a3ed8001cf928dcdfe3db6ad151e2348a47e
Victor Peinado <vpeinado263@gmail.com>
2025-09-03 21:44:49 -0300

    Agrega nuevos estados

--- frontend/src/contexts/DeviceContext.jsx
+++ frontend/src/contexts/DeviceContext.jsx
@@ -8,6 +8,8 @@ export const DeviceProvider = ({ children }) => {
   const [isOn, setIsOn] = useState(false);
   const [loading, setLoading] = useState(false);
   const [step, setStep] = useState(0);
+  const [inserted, setInserted] = useState(false);
+  const [closed, setClosed] = useState(false);
 
   const togglePower = () => {
     if (!isOn) {
@@ -15,27 +17,45 @@ export const DeviceProvider = ({ children }) => {
       setTimeout(() => {
         setIsOn(true);
         setLoading(false);
+        setStep(1);
       }, 3000);
     } else {
       setIsOn(false);
       setLoading(false);
-      setStep(0); // reiniciamos step al apagar
+      setStep(0);
+      setInserted(false);
+      setClosed(false);
     }
   };
 
   const nextStep = () => {
-    if (step === 0) {
-      setStep(1);
+    if (step === 1) {
       setTimeout(() => {
         setStep(2);
       }, 3000);
-    } else {
-      setStep(0);
+    } else if (step === 2 && inserted && closed) {
+      setStep(4);
+      setTimeout(() => {
+        setStep(5);
+      }, 3000);
     }
   };
 
   return (
-    <DeviceContext.Provider value={{ isOn, loading, step, setStep, togglePower, nextStep }}>
+    <DeviceContext.Provider
+      value={{
+        isOn,
+        loading,
+        step,
+        setStep,
+        togglePower,
+        nextStep,
+        inserted,
+        setInserted,
+        closed,
+        setClosed,
+      }}
+    >
       {children}
     </DeviceContext.Provider>
   );
