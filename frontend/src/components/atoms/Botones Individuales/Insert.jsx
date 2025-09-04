78e3567e156f365137db79f4203b30fe1c0c4b2c
Victor Peinado <vpeinado263@gmail.com>
2025-09-03 21:57:53 -0300

    Agregar estado a botón

--- frontend/src/components/atoms/Botones Individuales/Insert.jsx
+++ frontend/src/components/atoms/Botones Individuales/Insert.jsx
@@ -1,6 +1,23 @@
+import { useDevice } from "@/contexts/DeviceContext";
+
 const Insert = () => {
+  const { setInserted } = useDevice
   return (
-    <button className="w-40 h-12 bg-green-600 text-white font-bold rounded-md border-2 border-gray-800 shadow-md hover:bg-green-700 active:scale-95 active:shadow-inner transition">
+    <button 
+    onClick={() => setInserted(true)}
+    className="w-40 h-12 
+    bg-green-600 
+    text-white 
+    font-bold 
+    rounded-md 
+    border-2 
+    border-gray-800 
+    shadow-md 
+    hover:bg-green-700 
+    active:scale-95 
+    active:shadow-inner 
+    transition"
+    >
       Insertar PLUM set
     </button>
   );
