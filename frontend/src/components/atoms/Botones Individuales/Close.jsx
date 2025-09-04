cf354aa0cfe48d5caab4ca03430fd512a27638e9
Victor Peinado <vpeinado263@gmail.com>
2025-09-03 21:53:17 -0300

    Agregar estado a button close

--- frontend/src/components/atoms/Botones Individuales/Close.jsx
+++ frontend/src/components/atoms/Botones Individuales/Close.jsx
@@ -1,6 +1,21 @@
+import { useDevice } from "@/contexts/DeviceContext";
+
 const Close = () => {
+  const { setClosed } = useDevice
   return (
-    <button className="w-40 h-12 bg-red-600 text-white font-bold rounded-md border-2 border-gray-800 shadow-md hover:bg-red-700 active:scale-95 active:shadow-inner transition">
+    <button
+    onClick={() => setClosed(true)} 
+    className="w-40 h-12
+     bg-red-600
+      text-white 
+      font-bold 
+      rounded-md
+      border-2
+     border-gray-800 
+      shadow-md hover:bg-red-700
+      active:scale-95
+      active:shadow-inner transition"
+      >
       Close Lever
     </button>  
   );
