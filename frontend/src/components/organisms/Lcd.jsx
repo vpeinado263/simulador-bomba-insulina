a72f553bd02528399a02f1ffd37f613542f6967e
Victor Peinado <vpeinado263@gmail.com>
2025-09-03 22:49:36 -0300

    Modificar estados de pantalla

--- frontend/src/components/organisms/Lcd.jsx
+++ frontend/src/components/organisms/Lcd.jsx
@@ -7,61 +7,59 @@ import WorkingRegion from "../molecules/WorkingRegion";
 import StartupScreen from "./screens/StartupScreen";
 
 const Lcd = () => {
-  const { step, isOn, loading } = useDevice();
+  const { step, isOn, loading, inserted, closed } = useDevice();
 
   switch (step) {
     case 0:
-    if (!isOn && !loading) {
-      return <div className="bg-black w-64 h-60 rounded-md" />;
-    }
-  
-    if (loading) {
-      return (
-        <div>
-           <StartupScreen />
-        </div>
-      );
-      
-    }
-    case 1: 
-    return (
-      <ScreenLayout>
-          <div>
-          <StatusRegion text="SETUP" />
-        </div>
-        <div>
-          <WorkingRegion text1="Insert PLUM Set" text2="Close Lever" />
-        </div>
-        <div>
-          <MessageRegion />
-        </div>
-        <div>
-          <SoftkeyLabel />
-        </div>
-      </ScreenLayout>
-    );
-    //Estado encendido 
-    case 2:
-    if (isOn && !loading) {
+      if (!isOn && !loading) {
+        return <div className="bg-black w-64 h-60 rounded-md" />;
+      }
+      if (loading) {
+        return <StartupScreen />;
+      }
+    case 1:
       return (
         <ScreenLayout>
-          <div>
-            <StatusRegion />
-          </div>
-          <div>
-            <WorkingRegion />
-          </div>
-          <div>
-            <MessageRegion />
-          </div>
-          <div>
-            <SoftkeyLabel />
-          </div>
+          <StatusRegion text="SETUP" />
+          <WorkingRegion text1="Insert PLUM Set" text2="Close Lever" />
+          <MessageRegion />
+          <SoftkeyLabel />
         </ScreenLayout>
       );
-    }
+    case 2:
+      if (!inserted || !closed) {
+        return (
+          <ScreenLayout>
+            <StatusRegion text="SETUP" />
+            <WorkingRegion text1="Insert PLUM Set" text2="Close Lever" />
+            <MessageRegion />
+            <SoftkeyLabel />
+          </ScreenLayout>
+        );
+      } else {
+        return (
+          <ScreenLayout>
+            <StatusRegion text="SETUP" />
+            <WorkingRegion text1="Mechanism Initialization" text2="In Progress..." />
+            <MessageRegion />
+            <SoftkeyLabel />
+          </ScreenLayout>
+        );
+      }
+    case 5:
+      return (
+        <ScreenLayout>
+          <StatusRegion text="|A| STOPPED | STOPPED |B|" />
+          <WorkingRegion text1="0 ml/hr" text2="0 Vol inf. ml" />
+          <MessageRegion />
+          <SoftkeyLabel text1="Black Prime" text2="A" text3="B" text4="Options/Vol Inf" />
+        </ScreenLayout>
+      );
+
+    default:
+      return null;
   }
-  return null;
 };
 
 export default Lcd;
+
