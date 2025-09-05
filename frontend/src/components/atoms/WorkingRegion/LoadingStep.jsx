b68ebeba9b6241aba2579baae8cfb608237ac8c8
Victor Peinado <vpeinado263@gmail.com>
2025-09-05 01:01:52 -0300

    Crear componente LoadingStep

---
+++ frontend/src/components/atoms/WorkingRegion/LoadingStep.jsx
@@ -0,0 +1,16 @@
+const LoadingStep = () => {
+  return (
+    <div className="flex-1 flex flex-col justify-center items-center text-sm">
+      <div className="flex flex-col items-center mb-2">
+        <span>Ver: 13.43.00.001 2014/11/20</span>
+        <span>Copyright Hospira Inc.</span>
+      </div>
+      <div className="flex flex-col items-center font-bold">
+        <span>System Self Test</span>
+        <span>In Progress</span>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingStep;
