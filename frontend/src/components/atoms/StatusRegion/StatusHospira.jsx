7ee639256733757946e3f9764f2755c9e1038122
Victor Peinado <vpeinado263@gmail.com>
2025-09-04 22:57:28 -0300

    Crear componente StatusHospira

---
+++ frontend/src/components/atoms/StatusRegion/StatusHospira.jsx
@@ -0,0 +1,11 @@
+const StatusHospira = () => {
+  return (
+    <div className="flex items-center">
+      <span className="flex-[1] h-8 bg-green-300 flex justify-center border border-green-600 text-gray-600">
+        HOSPIRA Plum A+
+      </span>
+    </div>
+  );
+};
+
+export default StatusHospira;
