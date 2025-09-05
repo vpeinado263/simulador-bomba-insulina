c82f863a495ffc64d5e9100a852f10fd26e3541e
Victor Peinado <vpeinado263@gmail.com>
2025-09-05 00:11:40 -0300

    Agregar text-black font-bold

--- frontend/src/components/atoms/StatusRegion/StatusAB.jsx
+++ frontend/src/components/atoms/StatusRegion/StatusAB.jsx
@@ -1,10 +1,10 @@
 const StatusAB = () => {
   return (
     <div className="flex items-center text-xs">
-      <span className="flex-1 h-8 bg-green-300 flex justify-center items-center border border-green-600">A</span>
-      <span className="flex-1 h-8 bg-green-300 flex justify-center items-center border border-green-600">STOPPED</span>
-      <span className="flex-1 h-8 bg-green-300 flex justify-center items-center border border-green-600">STOPPED</span>
-      <span className="flex-1 h-8 bg-green-300 flex justify-center items-center border border-green-600">B</span>
+      <span className="flex-1 h-8 bg-green-300 flex justify-center items-center border border-green-600 text-black font-bold">A</span>
+      <span className="flex-3 h-8 bg-green-300 flex justify-center items-center border border-green-600 text-black font-bold">STOPPED</span>
+      <span className="flex-3 h-8 bg-green-300 flex justify-center items-center border border-green-600 text-black font-bold">STOPPED</span>
+      <span className="flex-1 h-8 bg-green-300 flex justify-center items-center border border-green-600 text-black font-bold">B</span>
     </div>
   );
 };
