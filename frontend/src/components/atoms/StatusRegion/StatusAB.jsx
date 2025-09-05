1c87d56ba29ef896748def845c32e0d9313476d0
Victor Peinado <vpeinado263@gmail.com>
2025-09-04 23:23:52 -0300

    Crear componente StatusAB

---
+++ frontend/src/components/atoms/StatusRegion/StatusAB.jsx
@@ -0,0 +1,12 @@
+const StatusAB = () => {
+  return (
+    <div className="flex items-center text-xs">
+      <span className="flex-1 h-8 bg-green-300 flex justify-center items-center border border-green-600">A</span>
+      <span className="flex-1 h-8 bg-green-300 flex justify-center items-center border border-green-600">STOPPED</span>
+      <span className="flex-1 h-8 bg-green-300 flex justify-center items-center border border-green-600">STOPPED</span>
+      <span className="flex-1 h-8 bg-green-300 flex justify-center items-center border border-green-600">B</span>
+    </div>
+  );
+};
+
+export default StatusAB;
