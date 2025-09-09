4171fa054e71862622b85e5dfc0a2fd99b6b31ea
Victor Peinado <vpeinado263@gmail.com>
2025-09-09 17:54:31 -0300

    Modificar texto de WorkingRegion

--- frontend/src/components/atoms/WorkingRegion/InsertStep.jsx
+++ frontend/src/components/atoms/WorkingRegion/InsertStep.jsx
@@ -1,13 +1,9 @@
 const InsertStep = () => {
   return (
     <div className="flex-1 flex flex-col justify-center items-center text-sm">
-            <div className="flex flex-col items-center mb-2">
-              <span>Ver: 13.43.00.001 2014/11/20</span>
-              <span>Copyright Hospira Inc.</span>
-            </div>
-            <div className="flex flex-col items-center font-bold">
-              <span>System Self Test</span>
-              <span>In Progress</span>
+            <div className="flex flex-col items-center mb-1">
+              <span>Insert PLUM Set</span>
+              <span>Close Lever</span>
             </div>
           </div>
   );
