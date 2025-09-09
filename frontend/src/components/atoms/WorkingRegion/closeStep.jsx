67fff29e561adb29cb7efb4fdb0314f04fb18f51
Victor Peinado <vpeinado263@gmail.com>
2025-09-09 18:24:34 -0300

    Modificar txto de componente

--- frontend/src/components/atoms/WorkingRegion/closeStep.jsx
+++ frontend/src/components/atoms/WorkingRegion/closeStep.jsx
@@ -2,12 +2,8 @@ const closeStep = () => {
   return (
     <div className="flex-1 flex flex-col justify-center items-center text-sm">
             <div className="flex flex-col items-center mb-2">
-              <span>Ver: 13.43.00.001 2014/11/20</span>
-              <span>Copyright Hospira Inc.</span>
-            </div>
-            <div className="flex flex-col items-center font-bold">
-              <span>System Self Test</span>
-              <span>In Progress</span>
+              <span>Mechanism Initialization</span>
+              <span>In Progress</span> 
             </div>
           </div>
   );
