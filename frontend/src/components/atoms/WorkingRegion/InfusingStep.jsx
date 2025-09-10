faa0a73266186809da886972843876ebbed8cd86
Victor Peinado <vpeinado263@gmail.com>
2025-09-10 18:44:37 -0300

    Modificar estilos de componente

--- frontend/src/components/atoms/WorkingRegion/InfusingStep.jsx
+++ frontend/src/components/atoms/WorkingRegion/InfusingStep.jsx
@@ -1,16 +1,24 @@
 const InfusingStep = () => {
   return (
-    <div className="flex-1 flex flex-col justify-center items-center text-sm">
-      <div className="flex flex-col items-center mb-2">
-        <span>00.0</span>
-        <span>00.0</span>
+    <div className="flex-1 flex flex-col justify-center items-center text-blue-950"> 
+      <div className="flex items-center gap-12 mb-3">
+        <span className="text-xl">0</span> 
+        <div className="flex flex-col items-center leading-tight">
+          <span className="text-sm">Rate</span>
+          <span className="text-xs">mL/h</span>
+        </div> 
+        <span className="text-xl">0</span>
       </div>
-      <div className="flex flex-col items-center font-bold">
-        <span>00.0</span>
-        <span>00.0</span>
+      <div className="flex items-center gap-11 mb-3">
+        <span className="text-xl">0</span>  
+        <div className="flex flex-col items-center leading-tight">
+          <span className="text-sm">VolInf</span>
+          <span className="text-xs">mL</span>
+        </div>   
+        <span className="text-xl">0</span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default InfusingStep
+export default InfusingStep;
