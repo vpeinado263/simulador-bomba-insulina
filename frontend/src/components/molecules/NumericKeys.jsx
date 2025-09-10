2ec55b0f0169430932c11b0b5f223b8eb3ab36f5
Victor Peinado <vpeinado263@gmail.com>
2025-09-09 21:19:59 -0300

    Agregar callback a componente padre

--- frontend/src/components/molecules/NumericKeys.jsx
+++ frontend/src/components/molecules/NumericKeys.jsx
@@ -1,11 +1,15 @@
 import ButtonNumeric from "../atoms/Botones Individuales/ButtonNumeric";
 
-const NumericKeys = () => {
+const NumericKeys = ({ onKeyPress }) => {
   const keys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "CLEAR", "0", "."];
   return (
     <div className="grid grid-cols-3 gap-4">
       {keys.map((key) => (
-        <ButtonNumeric key={key} label={key} />
+        <ButtonNumeric 
+        key={key} 
+        label={key} 
+        onClick={() => onKeyPress(key)}
+        />
       ))}
     </div>
   );
