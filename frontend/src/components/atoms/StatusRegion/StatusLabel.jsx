4b08922b9ca7c2fff2b3e3068f965a032fe5eec4
Victor Peinado <vpeinado263@gmail.com>
2025-09-10 10:50:49 -0300

    Agregar estilos necesarios

--- frontend/src/components/atoms/StatusRegion/StatusLabel.jsx
+++ frontend/src/components/atoms/StatusRegion/StatusLabel.jsx
@@ -1,5 +1,5 @@
 const StatusLabel = ({ children, flex = 1, bg = "bg-gren-300"}) => (
-    <div className={`${bg} flex justtify-center items-center border border-green-600 text-black text-xs h-8 flex-${flex}`}>
+    <div className={`${bg} flex justify-center items-center border border-green-600 text-black text-xs h-8 flex-${flex}`}>
       {children}
     </div>
 );
