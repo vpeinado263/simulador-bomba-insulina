504a91463621b67971ccb34e043772f98641e2ff
Victor Peinado <vpeinado263@gmail.com>
2025-09-21 11:38:51 -0300

    Crear componente Footer.jsx

---
+++ frontend/src/components/templates/Footer.jsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>
+        © 2025 Emulador Educativo Bomba de Infusion Plum A+. Todos los derechos reservados.
+        </p>
+    </footer>
+  );
+};
+
+export default Footer;
