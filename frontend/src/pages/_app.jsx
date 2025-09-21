57014996a92a16cd6b4b5c62dd0c14deb582564f
Victor Peinado <vpeinado263@gmail.com>
2025-09-20 23:32:21 -0300

    Importar componente Seo.jsx

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -3,6 +3,7 @@ import "../styles/globals.css";
 import { useState, useEffect } from "react";
 import SpinnerInicio from "@/components/atoms/SpinnerInicio";
 import { DeviceProvider } from "@/contexts/DeviceContext";
+import Seo from "@/components/seo/Seo";
 
 export default function App({ Component, pageProps }) {
   const [loading, setLoading] = useState(true);
@@ -18,6 +19,8 @@ export default function App({ Component, pageProps }) {
         <title>Bomba de Infusión</title>
         <link rel="icon" href="/nurshing.ico" />
       </Head>
+
+      <Seo />
 
       {loading ? (
         <SpinnerInicio />
