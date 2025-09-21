ce13c87c1fa81cab4c5cd69ab2fba290185bb973
Victor Peinado <vpeinado263@gmail.com>
2025-09-21 11:57:54 -0300

    Insertar estilos Footer.jsx

--- frontend/src/components/organisms/Footer.jsx
+++ frontend/src/components/organisms/Footer.jsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="w-full bg-blue-400 text-white py-4 text-center text-sm">
         <p>
         © 2025 Emulador Educativo Bomba de Infusion Plum A+. Todos los derechos reservados.
         </p>
